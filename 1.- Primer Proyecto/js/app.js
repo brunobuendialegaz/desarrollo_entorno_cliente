37e93dfe2c4433aaa3c6c4aee89df4c739711e48
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-09-15 18:37:17 +0200

    traer elementos

---
+++ 1.- Primer Proyecto/js/app.js
@@ -0,0 +1,9 @@
+console.log("aplicación iniciada correctamente");
+
+// seleccionar los elementos del dom
+
+const nombre = document.getElementById("nombre");
+
+const botonSaludar = document.querySelector("botonSaludar"); // este busca por clase, al ser singular, devuelve el primero, si fuera all, traería todos los que compartan esa clase, hay mas sintaxis, por ejemplo #nombre para IDS, es sintaxis de CSS
+
+const mensaje = document.getElementById("mensaje");
