400d9bdb14a4947f92d3762765bb6190e328b9d6
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-09-15 19:02:13 +0200

    Primer escuchador de eventos

--- 1.- Primer Proyecto/js/app.js
+++ 1.- Primer Proyecto/js/app.js
@@ -3,7 +3,15 @@ console.log("aplicación iniciada correctamente");
 // seleccionar los elementos del dom
 
 const nombre = document.getElementById("nombre");
-
-const botonSaludar = document.querySelector("botonSaludar"); // este busca por clase, al ser singular, devuelve el primero, si fuera all, traería todos los que compartan esa clase, hay mas sintaxis, por ejemplo #nombre para IDS, es sintaxis de CSS
-
+const botonSaludar = document.querySelector("#botonSaludar"); // este busca por clase, al ser singular, devuelve el primero, si fuera all, traería todos los que compartan esa clase, hay mas sintaxis, por ejemplo #nombre para IDS, es sintaxis de CSS
 const mensaje = document.getElementById("mensaje");
+
+botonSaludar.addEventListener("click", () => {
+  const nombreActual = nombre.value.trim();
+
+  if (nombreActual === "") {
+    mensaje.textContent = "Debes introducir un nombre";
+  } else {
+    mensaje.textContent = `Hola ${nombreActual}`;
+  }
+});
