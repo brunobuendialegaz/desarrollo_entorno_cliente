ecbf1f5d531c3827e69d8af878b08467d2fabd39
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-09-22 11:51:03 +0200

    boton reiniciar, funciones

--- 1.- Primer Proyecto/js/app.js
+++ 1.- Primer Proyecto/js/app.js
@@ -3,6 +3,7 @@ console.log("aplicación iniciada correctamente");
 // seleccionar los elementos del dom
 
 const nombre = document.getElementById("nombre");
+const botonReiniciar = document.getElementById("botonReiniciar");
 const botonSaludar = document.querySelector("#botonSaludar"); // este busca por clase, al ser singular, devuelve el primero, si fuera all, traería todos los que compartan esa clase, hay mas sintaxis, por ejemplo #nombre para IDS, es sintaxis de CSS
 const mensaje = document.getElementById("mensaje");
 
@@ -11,7 +12,21 @@ botonSaludar.addEventListener("click", () => {
 
   if (nombreActual === "") {
     mensaje.textContent = "Debes introducir un nombre";
-  } else {
-    mensaje.textContent = `Hola ${nombreActual}`;
+    return;
   }
+  mensaje.textContent = `Hola ${nombreActual}`;
+  mensaje.classList.add("destacado");
+});
+
+/*nombre.addEventListener("keydown", (evento) => {
+  if (evento.key === "Enter") {
+    saludar();
+  }
+});*/
+
+botonReiniciar.addEventListener("click", () => {
+  nombre.value = "";
+  mensaje.textContent = "Aquí aparecerá el mensaje";
+  mensaje.classList.remove("destacado");
+  nombre.focus();
 });
