278284f4aa89cc2c8c752f1d473c1c1d3784fa74
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-09-23 19:17:33 +0200

    trasteos varios

---
+++ 2.- Peladillos Eladio/app.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const nombrePelu = "Peladillos Eladio";
+
+let clientesAtendidos = 5;
+
+console.log("Clientes atendidos: ", clientesAtendidos);
+
+const precioCorte = 8;
+
+const precioBarba = 8.5;
+
+console.log("precioCorte", typeof precioCorte);
+
+console.log("precioBarba", typeof precioBarba);
